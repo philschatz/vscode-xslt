3a2f35107d3ca566ccc5172f841d3d5f97cc629e
Philip Schatz <253202+philschatz@users.noreply.github.com>
2020-04-01 09:33:04 -0500

    add tunnel params

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 
 import * as path from 'path'
 import { debug, DebugAdapterDescriptor, DebugAdapterDescriptorFactory, DebugAdapterExecutable, DebugSession, ExtensionContext, ProviderResult } from 'vscode'
+// import { DebugAdapterServer } from 'vscode'
 
 export function activate(context: ExtensionContext) {
   context.subscriptions.push(debug.registerDebugAdapterDescriptorFactory('xslt', new DAPFactory()))
@@ -13,10 +14,11 @@ class DAPFactory implements DebugAdapterDescriptorFactory {
   createDebugAdapterDescriptor(_session: DebugSession, executable: DebugAdapterExecutable | undefined): ProviderResult<DebugAdapterDescriptor> {
 		const command = 'java';
 		const args = [
-			// debugger argument: '-agentlib:jdwp=transport=dt_socket,address=127.0.0.1:8888,server=y,suspend=n'
+			// '-agentlib:jdwp=transport=dt_socket,address=127.0.0.1:8888,server=y,suspend=y',
 			'-jar',
 			path.join(__dirname, '..', 'server', `xslt-debug-adapter-0.1.1-all.jar`)
 		]
 		return new DebugAdapterExecutable(command, args)
+		// return new DebugAdapterServer(8129)
 	}  
 }
